c0177e4543e4d952e7f08b3d65a0e6b3e09be510
Alexandra Velichkova <alexandraavelichkova@gmail.com>
2025-02-07 11:09:57 +0200

    Added a calendar feature

--- app/agenda.js
+++ app/agenda.js
@@ -1,7 +1,7 @@
 // /agenda, /agenda/add, /agenda/edit/:id 
 
 const agendaRouter = require('express').Router();
-const {getItem, setItem} = require("./index")
+const {getItem, setItem} = require("../index")
 const { LocalStorage } = require('node-localstorage');
 const localStorage = new LocalStorage('./scratch');
 // const { data } = require('./data');
@@ -25,57 +25,78 @@ agendaRouter.get('/:year/:month/:day', (req, res) => {
     
         return totalMinutesA - totalMinutesB; 
     });
-    console.log(agendaSelectDate);
-    res.render('agenda', { agendaSelectDate });
+    // console.log(agendaSelectDate);
+    const action = `/agenda/${year}/${month}/${date}/add`;
+    const actionAdd = `/agenda/${year}/${month}/${date}/add`;
+    const actionEdit = `/agenda/${year}/${month}/${date}/edit`;
+    res.render('agenda', { agendaSelectDate, actionAdd, actionEdit });
     // module.exports = { fullDate };
 })
 
-agendaRouter.get('/add', (req, res) => {
+agendaRouter.get('/:year/:month/:day/add', (req, res) => {
     const method = 'Add';
-    const action = '/agenda/add';
+
+    const year = (+req.params.year);
+    const month = (+req.params.month);
+    const date = (+req.params.day);
+
+    const action = `/agenda/${year}/${month}/${date}/add`;
     res.render('upsert', { action, method });
 })
 
-agendaRouter.post('/add', (req, res) => {
+agendaRouter.post('/:year/:month/:day/add', (req, res) => {
     const time = req.body.time;
     const event = req.body.event;
     const type = req.body.type;
 
-    // to be modified for all dates
-
-    const date = new Date();
+    const year = (+req.params.year);
+    const month = (+req.params.month);
+    const date = (+req.params.day);
 
     const newEvent = {time: time, eventContent: event, type: type, index: data.length, 
-        isChecked: false, year: date.getFullYear(), month: (date.getMonth() + 1), date: date.getDate()};
+        isChecked: false, year: year, month: month, date: date};
     data.push(newEvent);
 
-    res.redirect('/agenda');
+    res.redirect(`/agenda/${year}/${month}/${date}`);
     saveAgenda();
 })
 
-agendaRouter.get('/edit/:id', (req, res) => {
-    const oldTime = data[req.params.id - 1].time;
-    const oldEvent = data[req.params.id - 1].eventContent;
-    const oldType = data[req.params.id - 1].type;
+agendaRouter.get('/:year/:month/:day/edit/:id', (req, res) => {
+    const event = data.find(e => e.index === +req.params.id);
+
+    const oldTime = event.time;
+    const oldEvent = event.eventContent;
+    const oldType = event.type;
+
+    const year = (+req.params.year);
+    const month = (+req.params.month);
+    const date = (+req.params.day);
+
     const method = 'Edit';
-    const action = '/agenda/edit/' + req.params.id;
+    const action = `/agenda/${year}/${month}/${date}/edit/` + req.params.id;
 
     res.render('upsert', {action, oldTime, oldEvent, oldType, method})
 })
 
-agendaRouter.post('/edit/:id', (req, res) => {
+agendaRouter.post('/:year/:month/:day/edit/:id', (req, res) => {
     const time = req.body.time;
     const event = req.body.event;
     const type = req.body.type;
 
-    const eventToEdit = data[req.params.id - 1];
+    const id = +req.params.id;
+
+    const year = (+req.params.year);
+    const month = (+req.params.month);
+    const date = (+req.params.day);
+
+    const eventToEdit = data.find(e => e.index === id);
 
     eventToEdit.time = time;
     eventToEdit.eventContent = event;
     eventToEdit.type = type;
     saveAgenda();
 
-    res.redirect('/agenda');
+    res.redirect(`/agenda/${year}/${month}/${date}`);
 })
 
 agendaRouter.delete('/delete/:id', (req, res) => {
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage('./scratch');
+// const { agendaSelectDate } = require('./static/calendar');
 // const { data } = require('./data')
 
 const app = express();
@@ -12,13 +15,22 @@ app.use(express.json())
 app.use(express.static(path.resolve('static')))
 app.use(express.static('styles'))
 
-app.get('/home', (req, res) => { // /home/:user-id 
+// app.get('/:year/:month/:day', (req, res) => {
+//     res.send(`${req.params.year}/${req.params.month}/${req.params.day}`);
+// })
+
+app.get('/home', (req, res) => {
+    // const data =  JSON.parse(localStorage.getItem('agenda'));// /home/:user-id 
     res.render("index");
+    // console.log(selectedDay);
+    //console.log(JSON.parse(localStorage.getItem('agenda')));
 })
 
-const agendaRouter = require("./agenda");
+const agendaRouter = require("./app/agenda");
 app.use("/agenda", agendaRouter);
 
 app.listen(3030, () => {
     console.log('Server listening on port 3030');
 })
+
+// module.exports = {getItem: localStorage.getItem, setItem: localStorage.setItem} ;
